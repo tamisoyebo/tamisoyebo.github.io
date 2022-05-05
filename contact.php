254816c79d0e5dc4b3e50d6725501947535c482c
tamisoyebo <tamisoyebo@gmail.com>
2022-05-04 22:33:57 -0400

    Update contact.php

--- contact.php
+++ contact.php
@@ -3,7 +3,7 @@ if (isset($_POST['Email'])) {
 
     // EDIT THE FOLLOWING TWO LINES:
     $email_to = "tamisoyebo@gmail.com";
-    $email_subject = "New form submission";
+    $email_subject = "New form submissions";
 
     function problem($error)
     {
@@ -16,16 +16,16 @@ if (isset($_POST['Email'])) {
 
     // validation expected data exists
     if (
-        !isset($_POST['Name']) ||
-        !isset($_POST['Email']) ||
-        !isset($_POST['Message'])
+        !isset($_POST['name']) ||
+        !isset($_POST['email']) ||
+        !isset($_POST['message'])
     ) {
         problem('We're sorry, but there appears to be a problem with the form you submitted.');
     }
 
-    $name = $_POST['Name']; // required
-    $email = $_POST['Email']; // required
-    $message = $_POST['Message']; // required
+    $name = $_POST['name']; // required
+    $email = $_POST['email']; // required
+    $message = $_POST['message']; // required
 
     $error_message = "";
     $email_exp = '/^[A-Za-z0-9._%-]+@[A-Za-z0-9.-]+\.[A-Za-z]{2,4}$/';
@@ -38,11 +38,11 @@ if (isset($_POST['Email'])) {
 
     if (!preg_match($string_exp, $name)) {
         $error_message .= 'The Name you entered does not appear to be valid.<br>';
-    }
+    
 
     if (strlen($message) < 2) {
         $error_message .= 'The Message you entered do not appear to be valid.<br>';
-    }
+    
 
     if (strlen($error_message) > 0) {
         problem($error_message);
@@ -56,9 +56,9 @@ if (isset($_POST['Email'])) {
         return str_replace($bad, "", $string);
     }
 
-    $email_message .= "Name: " . clean_string($name) . "\n";
-    $email_message .= "Email: " . clean_string($email) . "\n";
-    $email_message .= "Message: " . clean_string($message) . "\n";
+    $email_message .= "name: " . clean_string($name) . "\n";
+    $email_message .= "email: " . clean_string($email) . "\n";
+    $email_message .= "message: " . clean_string($message) . "\n";
 
     // create email headers
     $headers = 'From: ' . $email . "\r\n" .
